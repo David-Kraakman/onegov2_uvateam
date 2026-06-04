5058202ce4a0e3eab4191235716fcc2a18f0d6e8
The_Creator <julesnino09@gmail.com>
2026-06-04 14:20:52 +0200

    Front end werkt bijna, maar er zijn nog een paar kleine dingen die moeten worden aangepast. De meeste van deze aanpassingen hebben te maken met de styling van de pagina en het toevoegen van enkele interactieve elementen. Zodra deze aanpassingen zijn gemaakt, zal de front end klaar zijn voor gebruik.

---
+++ SickSim/src/data/utrechtData.ts
@@ -0,0 +1,11 @@
+export const utrechtData = {
+  location: 'Utrecht',
+  neighborhoods: [],
+  cbsTables: {
+    '86165NED': [],
+    '82275NED': [],
+    '70262NED': [],
+    '85870NED': [],
+  },
+  rwzi: [],
+};
